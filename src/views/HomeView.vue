7949e277869f5100459ff5cf30840d4346d90c5b
Simp4Hitagi <denorauten@gmail.com>
2023-03-14 16:54:11 +0200

    Loader and Preview added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <h1 class="display-1 my-5 p-5 animate__animated animate__fadeInRightBig">Welcome to Mangako</h1>
     <h2 class="display-3 mx-5 my-5 text-center animate__animated animate__fadeInLeftBig">Your ONE-STOP Shop for Manga</h2>
+    <h2 class="display-5 mx-5 my-5 text-center animate__animated animate__fadeInRightBig">Preview</h2>
     <div class="book w-25 h-50 mx-auto">
       <div class="book-cover">
         
