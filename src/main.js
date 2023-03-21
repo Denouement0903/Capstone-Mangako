5b08b5d43cfe8184975178d424a59efe24a922b5
Simp4Hitagi <denorauten@gmail.com>
2023-03-21 06:29:38 +0200

    firebase

--- src/main.js
+++ src/main.js
@@ -4,4 +4,23 @@ import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCW6a0RngH1tr7er22QvcPBL83V18mvkV8",
+  authDomain: "mangako-deno.firebaseapp.com",
+  projectId: "mangako-deno",
+  storageBucket: "mangako-deno.appspot.com",
+  messagingSenderId: "395959660421",
+  appId: "1:395959660421:web:00c42a47b777eeb8a05eec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 createApp(App).use(store).use(router).mount('#app')
