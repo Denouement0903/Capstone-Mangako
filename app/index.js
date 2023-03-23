7c76e69f9bd9c985249b6f1545c97eec489e1ae0
Simp4Hitagi <denorauten@gmail.com>
2023-03-23 16:44:37 +0200

    updates

--- app/index.js
+++ app/index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res, next) => {
 
 app.use( 
     (req, res, next)=> {
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        // res.setHeader('Access-Control-Allow-Origin', '*');
         res.header("Access-Control-Allow-Credentials", "true")
         res.header("Access-Control-Allow-Methods", "*")
         res.header("Access-Control-Allow-Headers", "*")
